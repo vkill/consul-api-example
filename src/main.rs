6cf495ee8fb80941d838092edb53bc232dd9e656
vkill <vkill.net@gmail.com>
2021-07-02 10:04:25 +0800

    Upgrade consul-api

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::error;
 
 use consul_api::{
-    api::catalog::{DatacentersEndpoint, NodesEndpoint},
+    api::catalog::{ListDatacenters, ListNodes},
     Client as _, IsahcClient,
 };
 
@@ -10,11 +10,11 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
     let mut client = IsahcClient::new().unwrap();
     client.set_base_url("http://127.0.0.1:8500/");
 
-    let endpoint = DatacentersEndpoint::new();
+    let endpoint = ListDatacenters::new();
     let res = client.respond_endpoint(&endpoint).await?;
     println!("datacenters {:?}", res);
 
-    let endpoint = NodesEndpoint::new();
+    let endpoint = ListNodes::new();
     let res = client.respond_endpoint(&endpoint).await?;
     println!("nodes {:?}", res);
 
